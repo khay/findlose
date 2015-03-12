eb50e4fd3c687a0efaa67ab6796dbc0d20229384
K <khayusaki@gmail.com>
2015-03-12 22:01:57 +0800

    Update libs

---
+++ libs/main.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+
+// Connect to mongodb database
+mongoose.connect('mongodb://localhost/pagewerkz')
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+
+// Router configuration
+var router = express.Router();
+var routes = require('./routes')(app);
+
+app.use('/api', router);
+
+/**
+ * To caught exceptions, so that the server won't stop for most errors
+ * Remove comments to enable it
+ */
+//process.on('uncaughtException', function(err) {
+//	console.log('Caught exception: ' + err);
+//});
+
+app.listen(3000);
