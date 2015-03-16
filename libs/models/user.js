771e8ae85c8201c0743abe62b0f321e133877096
K <khayusaki@gmail.com>
2015-03-16 18:00:26 +0800

    Add authentication with passport for user

--- libs/models/user.js
+++ libs/models/user.js
@@ -2,9 +2,8 @@
 
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
-var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
+var userSchema = new mongoose.Schema({
 	email: {
 		type: String,
 		required: true,
@@ -23,5 +22,31 @@ var userSchema = new Schema({
 		required: true
 	}
 });
+
+// Execute before each user.save() call
+UserSchema.pre('save', function(callback) {
+	var user = this;
+
+	// Break out if the password hasn't changed
+	if (!user.isModified('password')) return callback();
+
+	// Password changed so we need to hash it
+	bcrypt.genSalt(5, function(err, salt) {
+    	if (err) return callback(err);
+
+    	bcrypt.hash(user.password, salt, null, function(err, hash) {
+      	if (err) return callback(err);
+      		user.password = hash;
+      		callback();
+    	});
+  	});
+});
+
+UserSchema.methods.verifyPassword = function(password, cb) {
+  	bcrypt.compare(password, this.password, function(err, isMatch) {
+    	if (err) return cb(err);
+    	cb(null, isMatch);
+  	});
+};
 
 module.exports = mongoose.model('User', userSchema);
--- libs/routes.js
+++ libs/routes.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var authController = require('./controllers/auth');
 var userController = require('./controllers/user');
 
 router.route('/user')
-    .get(userController.getUser)
+    .get(authController.isAuthenticated, userController.getUser)
     .post(userController.postUser);
 
 module.exports = function(app) {
