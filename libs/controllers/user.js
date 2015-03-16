ab2af61134c480146e8474a14f18eb8a6eabe3ae
K <khayusaki@gmail.com>
2015-03-16 17:52:28 +0800

    Add oauth2orize. Update user controller

--- libs/controllers/user.js
+++ libs/controllers/user.js
@@ -1,7 +1,34 @@
 // libs/controllers/user.js
-
 var User = require('../models/user');
 
-exports.getUser = function(req, res) { res.json({message: 'ok!'}) };
+// Create endpoint /api/user for GET
+exports.getUser = function(req, res) {
+	// Find all users in the database
+	// This is just a temporary
+	User.find({}, function(err, users) {
+		if (err)
+			res.send(err);
 
-exports.postUser = function(req, res) {};
+		res.json(users);
+	});
+};
+
+// Create endpoint /api/user for POST
+exports.postUser = function(req, res) {
+	// Create a new Instance of the User model
+	var user = new User();
+
+	// Set the user properties that came from the POST data
+	user.email = req.body.email;
+	user.displayname = req.body.displayname;
+	user.password = req.body.password;
+	user.contactNumber = req.body.contactNumber;
+
+	// Save the user and check for errors
+	user.save(function(err) {
+		if (err)
+	  		res.send(err);
+
+		res.json({ message: 'New user has been added!' });
+	});
+};
